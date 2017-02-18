f8907d1d17c497214a44caa9f040518643eb3eba
simonepozzobon <info@simonepozzobon.com>
2017-02-18 01:51:01 +0100

    Admin > Add post > Form

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -6,7 +6,9 @@
   <body>
     <div class="container">
       @include('admin.menu')
+
       <h1>Create a new post</h1>
+      {{ Html::ul($errors->all()) }}
 
       {!! Form::open(['url' => 'admin/posts']) !!}
 
@@ -15,10 +17,22 @@
         {{ Form::text('title', Input::old('title'), ['class' => 'form-control']) }}
       </div>
 
+      <div class="form-group">
+        {{ Form::label('content', 'Content') }}
+        {{ Form::text('content', Input::old('content'), ['class' => 'form-control']) }}
+      </div>
+
+      <div class="form-group">
+        {{ Form::label('content', 'Content') }}
+        {{ Form::text('content', Input::old('content'), ['class' => 'form-control']) }}
+      </div>
+
+        {{ Form::submit('Create Post', ['class' => 'btn btn-primary']) }}
 
       {!! Form::close() !!}
 
     </div>
+
     @include('admin.footer')
   </body>
 </html>
