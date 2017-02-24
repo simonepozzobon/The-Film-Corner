dc6d8d3aeb796853a59fdb3d6ebd1c246c9f7c72
simonepozzobon <simonepozzobon@Esse.local>
2017-02-24 01:08:08 +0100

    Admin > thumbnail creation
    Need to increase file size limit. Not working with images bigger than
    2MB

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Media;
 use App\Http\Requests\StoreMedia;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 class MediaController extends Controller
 {
@@ -38,11 +40,36 @@ class MediaController extends Controller
      */
     public function store(StoreMedia $request)
     {
-        $file = $request->file('media')->store('public/media');
+        $filename = $request->file('media')->getClientOriginalName();
+        $file = $request->file('media')->storeAs('public/media', $filename);
+        //$img = Image::make($request->file('media')->getRealPath());
+
+        $path = $request->file('media');
+        // Resize with image intervention
+        $img_icon = Image::make($path)->resize( 300, null, function ($constraint) {
+          $constraint->aspectRatio();
+        });
+        // Path where resized file are stored
+        $store_path = public_path('storage/media/' . 'icon_'.$filename);
+        // save action
+        $store = $img_icon->save($store_path);
+
+        //$img_icon = $img->resize(57,57)->storeAs('public/media', 'icon_'.$ilename);
+        //$img_square = $img->resize(400,400)->storeAs('public/media', 'square_'.$ilename);
+        //$img_mobile = $img->resize(320, null)->storeAs('public/media', 'mobile_'.$ilename);
+        //$img_tablet = $img->resize(1024, null)->storeAs('public/media', 'tablet_'.$ilename);
+        //$img_big = $img->resize(1920,null)->storeAs('public/media', 'big_'.$ilename);
+
+        // Run the query
         $media = new Media;
         $media->title = $request->input('title');
         $media->url = $file;
         $media->alt = $request->input('alt');
+        $media->icon = $store_path;
+        $media->square = $store_path;
+        $media->mobile = $store_path;
+        $media->tablet = $store_path;
+        $media->big = $store_path;
         $media->save();
 
         return redirect('/admin/media')->with('status', 'New media uploaded!');
