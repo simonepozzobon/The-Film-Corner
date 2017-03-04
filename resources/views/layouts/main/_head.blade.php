ed69c44fc299e89a3a794c4798ea3a92e49eb2d4
simone pozzobon <simonepozzobon@Esse-002.homenet.telecomitalia.it>
2017-03-04 14:56:40 +0100

    Google Analytics Metrics

--- resources/views/layouts/main/_head.blade.php
+++ resources/views/layouts/main/_head.blade.php
@@ -17,6 +17,18 @@
     {{-- Main Stylesheet --}}
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
+    {{-- Google Analytics --}}
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-92981271-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+
     <style media="screen">
 
     html, body {
