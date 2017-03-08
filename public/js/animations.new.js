60455e6c17940afd14e8f8f2f550b0c5bf29e7b0
simone pozzobon <simonepozzobon@Esse.homenet.telecomitalia.it>
2017-03-08 14:02:45 +0100

    Animations

--- public/js/animations.new.js
+++ public/js/animations.new.js
@@ -58,13 +58,71 @@
           }
         });
 
+        // Project Section Scene
+        var scrollElemToWatch_2 = document.getElementById('project-title'),
+          watcher_2 = scrollMonitor.create(scrollElemToWatch_2, -300),
+          rev4 = new RevealFx(scrollElemToWatch_2, {
+            revealSettings : {
+              bgcolor: '#C0A03D',
+              direction: 'bt',
+              onCover: function(contentEl, revealerEl) {
+                contentEl.style.opacity = 1;
+              }
+            }
+          }),
+          rev5 = new RevealFx(document.querySelector('#project-container'), {
+            revealSettings : {
+              bgcolor: '#404040',
+              direction: 'rl',
+              delay: 500,
+              onCover: function(contentEl, revealerEl) {
+                contentEl.style.opacity = 1;
+              }
+            }
+          });
 
+          // Project Section Scene
+          var scrollElemToWatch_3 = document.getElementById('partners-title'),
+            watcher_3 = scrollMonitor.create(scrollElemToWatch_3, -300),
+            rev6 = new RevealFx(scrollElemToWatch_3, {
+              revealSettings : {
+                bgcolor: '#C0A03D',
+                direction: 'bt',
+                onCover: function(contentEl, revealerEl) {
+                  contentEl.style.opacity = 1;
+                }
+              }
+            }),
+            rev7 = new RevealFx(document.querySelector('#partners-container'), {
+              revealSettings : {
+                bgcolor: '#404040',
+                direction: 'lr',
+                delay: 500,
+                onCover: function(contentEl, revealerEl) {
+                  contentEl.style.opacity = 1;
+                }
+              }
+            });
 
         // News Section Init
         watcher_1.enterViewport(function() {
           rev2.reveal();
           rev3.reveal();
           watcher_1.destroy();
+        });
+
+        // Project Section Init
+        watcher_2.enterViewport(function() {
+          rev4.reveal();
+          rev5.reveal();
+          watcher_2.destroy();
+        });
+
+        // Project Section Init
+        watcher_3.enterViewport(function() {
+          rev6.reveal();
+          rev7.reveal();
+          watcher_3.destroy();
         });
 
     }
