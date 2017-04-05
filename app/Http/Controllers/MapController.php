32368e76d53fc8dcffe30679cb94946d1be85646
simone pozzobon <simonepozzobon@Esse.station>
2017-04-05 16:14:26 +0200

    Style map

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -2,67 +2,13 @@
 
 namespace App\Http\Controllers;
 
-
-use Ivory\GoogleMap\Base\Bound;
-use Ivory\GoogleMap\Base\Coordinate;
-use Ivory\GoogleMap\Control\ControlPosition;
-use Ivory\GoogleMap\Control\MapTypeControl;
-use Ivory\GoogleMap\Control\MapTypeControlStyle;
-use Ivory\GoogleMap\MapTypeId;
-use Ivory\GoogleMap\Layer\GeoJsonLayer;
-use Ivory\GoogleMap\Helper\Builder\ApiHelperBuilder;
-use Ivory\GoogleMap\Helper\Builder\MapHelperBuilder;
-use Ivory\GoogleMap\Helper\Builder\PlaceAutocompleteHelperBuilder;
-use Ivory\GoogleMap\Map;
-use Ivory\GoogleMap\Place\Autocomplete;
 use Illuminate\Http\Request;
 
 class MapController extends Controller
 {
     public function index()
     {
-      $map = new Map();
-      $map->setVariable('map');
-      $map->setHtmlId('map_canvas');
-      // Disable the auto zoom flag (disabled by default)
-      $map->setAutoZoom(false);
-
-      // Sets the center
-      $map->setCenter(new Coordinate(45.464072, 9.190453));
-
-      // Set map option
-      $map->setMapOption('zoom', 14);
-      $map->setMapOption('streetViewControl', false);
-
-      $map->setStylesheetOption('width', '100%');
-      $map->setStylesheetOption('height', '100%');
-
-      // Create control options
-      $mapTypeControl = new MapTypeControl(
-          [MapTypeId::ROADMAP],
-          ControlPosition::TOP_RIGHT,
-          MapTypeControlStyle::DEFAULT_
-      );
-      // Append to map object
-      $map->getControlManager()->setMapTypeControl($mapTypeControl);
-
-      $autocomplete = new Autocomplete();
-      $autocompleteHelper = PlaceAutocompleteHelperBuilder::create()->build();
-      $autocompleteRender = $autocompleteHelper->render($autocomplete);
-
-      // build the map
-      $mapHelper = MapHelperBuilder::create()->build();
-
-      // Set The Api
-      $apiHelper = ApiHelperBuilder::create()->setKey('AIzaSyBm2lmdXnrE3nfANgjfV_7d_cE35A0iGos')->build();
-      $apiHelperRender = $apiHelper->render([$autocomplete]);
-
-      $mapRender = $mapHelper->render($map);
-      echo $apiHelper->render([$map]);
-
-
-      return view('map.index')
-            ->with('map', $mapRender);
+      return view('map.index');
     }
 
 }
--- resources/views/map/index.blade.php
+++ resources/views/map/index.blade.php
@@ -2,9 +2,162 @@
 @section('title', 'Spot Map')
 @section('stylesheets')
   <link rel="stylesheet" href="{{ asset('css/maps/style.css') }}">
+  <style media="screen">
+  /* Always set the map height explicitly to define the size of the div
+    * element that contains the map. */
+   #map {
+     height: 100%;
+   }
+   /* Optional: Makes the sample page fill the window. */
+   html, body {
+     height: 100%;
+     margin: 0;
+     padding: 0;
+   }
+  </style>
 @endsection
 @section('content')
   <div class="map-wrapper">
-    {!! $map !!}
+    <div id="map"></div>
   </div>
+@endsection
+@section('scripts')
+  <script>
+      function initMap() {
+        // Create a map object and specify the DOM element for display.
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 45.464167, lng: 9.190027},
+          scrollwheel: false,
+          zoom: 14,
+          disableDefaultUI: true,
+          zoomControl: true,
+          streetViewControl: false,
+          mapTypeControlOptions: {
+            mapTypeIds: ['roadmap']
+          }
+        });
+
+        map.setOptions({
+          styles: [
+            // Hide Features
+            {
+              featureType: 'poi.business',
+              stylers: [{visibility: 'off'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'labels.icon',
+              stylers: [{visibility: 'off'}]
+            },
+            {
+              featureType: 'road.arterial',
+              elementType: 'labels.icon',
+              stylers: [{visibility: 'off'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.icon',
+              stylers: [{visibility: 'off'}]
+            },
+            // Styles Features
+            {elementType: 'geometry', stylers: [{color: '#FFF5D6'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#FFF5D6'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#FAD86A'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#FAD86A'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#FAD86A'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#F8E9B0'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#FAD86A'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#F8E9B0'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#F8E9B0'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#FAD86A'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#FAD86A'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#F8E9B0'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#FAD86A'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#F8E9B0'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#FFE47D'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+          ]
+        });
+      }
+
+
+      // Resize Window
+      function setSize(a) {
+        var h = a-140;
+        $('.map-wrapper').height(h);
+      };
+
+      var h = $(window).height();
+      setSize(h);
+
+      $(window).resize(function() {
+        var h = $(window).height();
+        setSize(h);
+      });
+
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBMuYp_fLHyQ-vkDFpJzLdS6WoU_uYSBHs&callback=initMap"
+    async defer></script>
 @endsection
