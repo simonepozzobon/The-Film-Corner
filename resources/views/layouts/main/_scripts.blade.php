e54ed7225af9c8b243916c6e16e35ca7115ff6fe
simone pozzobon <simonepozzobon@Esse.station>
2017-04-10 15:30:45 +0200

    Fix bootstrap js

--- resources/views/layouts/main/_scripts.blade.php
+++ resources/views/layouts/main/_scripts.blade.php
@@ -1,8 +1,12 @@
-<script src="//code.jquery.com/jquery-3.1.1.min.js" crossorigin="anonymous"></script>
+{{-- <script src="//code.jquery.com/jquery-3.1.1.min.js" crossorigin="anonymous"></script>
 
 <script src="//cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" crossorigin="anonymous"></script>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" crossorigin="anonymous"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" crossorigin="anonymous"></script> --}}
+
+<script src="{{ asset('js/jquery-3.2.0.min.js') }}"></script>
+<script src="{{ asset('js/tether.min.js') }}"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
 
 {{-- Script --}}
-<script src="{{ asset('js/custom.js') }}"></script>
+{{-- <script src="{{ asset('js/app.js') }}"></script> --}}
 @yield('scripts')
