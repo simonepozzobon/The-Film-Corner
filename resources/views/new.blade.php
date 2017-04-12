40ebbb8eac2637e9cb4beb610030b1ae9cc5759f
simone pozzobon <simonepozzobon@Esse.station>
2017-04-12 14:16:22 +0200

    Home Fixes

--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -320,7 +320,7 @@
                               </div>
                             </div>
                             <div class="modal-footer">
-                              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                              <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
                             </div>
                           </div>
                         </div>
