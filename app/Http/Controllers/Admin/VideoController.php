940c738905b1772c14baa7d826d76c9b6c045e97
simone pozzobon <info@simonepozzobon.com>
2017-05-02 18:49:26 +0200

    Video Frontend
    - Add new session when page load.
    - Create new empty session each load.
    - Create a still frame video with editor image

--- app/Http/Controllers/Admin/VideoController.php
+++ app/Http/Controllers/Admin/VideoController.php
@@ -4,16 +4,44 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 use App\VideoLibrary;
-use FFMpeg; //php-FFmpeg
+use App\Video;
 
 class VideoController extends Controller
 {
     public function index()
     {
+      // definisco la library di FFMPEG
+      define('FFMPEG_LIB', '/usr/local/bin/ffmpeg');
+
+      // creo una nuova sessione
+      $img = public_path('img/helpers/poster.png');
+
+      // Definisco il codice della sessione
+      $session_id = uniqid();
+
+      // Creo il video della sessione e lo salvo nella cartella /video/sessions/id della sessione
+      $storePath = storage_path('app/public/video/sessions/'.$session_id);
+
+      $mkdir = Storage::makeDirectory('public/video/sessions/'.$session_id, 0777, true);
+
+      $cli = FFMPEG_LIB.' -r 25 -f image2 -s 1920x1080 -y -i '.$img.' -vframes 125 -vcodec libx264 -crf 25 -pix_fmt yuv420p '.$storePath.'/tfc_video_session.mp4';
+      exec($cli);
+
+      // definisco la path pubblica per recuperare il video successivamente
+      $publicPath = 'storage/video/sessions/'.$session_id;
+
+      // Salvo la sessione nel database
+      $Video = new Video;
+      $Video->session = $session_id;
+      $Video->media_url = $publicPath.'/tfc_video_session.mp4';
+      $Video->save();
+
       // prendo la libreria
       $elements = VideoLibrary::all();
-      return view('video.index', compact('elements'));
+
+      return view('video.index', compact('elements', 'session_id'));
     }
 
     public function upload(Request $request)
