c570b2ef69a5ab205c012674953cf121bd6288ec
simone pozzobon <info@simonepozzobon.com>
2017-05-15 13:14:13 +0200

    Download section

--- resources/views/public/conference/download.blade.php
+++ resources/views/public/conference/download.blade.php
@@ -6,4 +6,39 @@
   <link rel="stylesheet" href="{{ asset('css/home.css') }}">
 @endsection
 @section('content')
+  <div class="block-subtitle mt-5">
+    <h4>Downloads</h4>
+  </div>
+  <div class="block-text mb-5">
+    <table class="table table-hover">
+      <thead>
+        <th>Description</th>
+        <th>Download</th>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="align-middle">General statement</td>
+          <td class="align-middle">
+            <a href="#" class="btn btn-primary">
+              <i class="fa fa-download" aria-hidden="true"></i> Download
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td class="align-middle">Invitation</td>
+          <td class="align-middle">
+            <div class="btn-group">
+              <a href="{{ asset('downloads/conference/The_Film_Corner_-_International_Conference_Invitation.pdf') }}" target="_blank" class="btn btn-primary">
+                <i class="fa fa-download" aria-hidden="true"></i> Download
+              </a>
+              <a href="{{ route('conference.application') }}" class="btn btn-info">
+                <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Online Application
+              </a>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+   </div>
+  </div>
 @endsection
