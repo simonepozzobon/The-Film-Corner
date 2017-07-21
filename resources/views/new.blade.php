c688c3ebdbd81d77b7694caa3243385cb1e71800
simone pozzobon <info@simonepozzobon.com>
2017-07-21 18:03:29 +0200

    Home update

--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -195,10 +195,14 @@
                   <div class="col-md-8">
                     <p class="text-justify pb-5">
                       “THE FILM CORNER. Online and offline activities for Film Literacy” project is aimed to the design, release and testing of an online digital virtual user-centered platform for Film Literacy, taking advantage of the opportunities offered by web 2.0 and crossmedia innovative approach in the digital era in order to raise the average film literacy level of EU young audiences. The general aim of the project is to contribute to draw an easy-going model for Film Literacy that could improve Film Literacy skills among the audience in order to foster Audience Development and Engagement towards film as an art form, with a particular focus on young and non-core audience.<br>
-                      The project involves 5 European institutions in 4 EU countries: Fondazione Cineteca Italiana in Milan, Italy (leading partner); The Film Space, an innovative Film Literacy provider (Manchester, UK); The Nerve Centre, a Creative Learning Centre Derry, Northern Ireland; the National Cinèmatheque of Serbia (Belgrade, Serbia) one of the eldest cinèmatheques in the world and the University of Milano Bicocca,  Dipartimento di Scienze Umane per la Formazione “Riccardo Massa” (Milan, Italy).<br>
                       The platform consists of an interactive narrative-based environmental layout with game-based didactical resources integrated as apps the user can interact with. Didactical resources will be both based on generic Film Literacy skills and on a set of EU national and non-national films. The platform is to be developed in at least 4 EU languages including english, french, italian and serbian.<br>
                       In november 2017, an international conference will take place in the frame of the 10th edition of “Piccolo Grande Cinema” Festival, a film festival dedicated to young audience promoted by Fondazione Cineteca Italiana. During the international conference the platform blueprint will be officially presented and some panels discussing several issues concerning Film Literacy, cross-media and Audience Development and engagement will be discussed. A call for papers will be issued and keynote speakers, including representants from other film literacy project financed in the frame of the Creative Europe Programme will be invited.<br>
                     </p>
+                    <ul>
+                      <li>PROJECT TITLE: The Film Corner. New online and offline activities for Film Literacy</li>
+                      <li>CONTRACT REFERENCE: 2016-2127/001-001-577573-CREA-1-2016-1-IT-MED-AUDEV</li>
+                      <li>LEADING INSTITUTION: Fondazione Cineteca Italiana, Milan, Italy</li>
+                    </ul>
                   </div>
                 </div>
               </div>
