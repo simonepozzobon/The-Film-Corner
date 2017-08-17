8a1cb38b96f63cdb3240feb06d7f500fb28f7b53
simone pozzobon <info@simonepozzobon.com>
2017-08-17 17:05:28 +0200

    New utility function to save, convert and extract image from video

--- app/Utility.php
+++ app/Utility.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Utility extends Model
 {
@@ -41,5 +42,63 @@ class Utility extends Model
     }
 
     return false;
+  }
+
+  /*
+   *
+   * $filename = nome del file senza estensione
+   * $ext = estensione del file originale
+   * $destFolder = video/uploads/ cartella di destinazione con / alla fine
+   *
+   * ritorna la path dell'immagine catturata a metà video e la path del video
+   *
+  */
+  public function storeVideo($file, $filename, $ext, $destFolder)
+  {
+    // definisco la library di FFMPEG
+    define('FFMPEG_LIB', '/usr/local/bin/ffmpeg');
+
+    // definisco la path global
+    $globalPath = Storage::disk('local')->getDriver()->getAdapter();
+
+    $file = $file->storeAs('public/video/tmp', $filename.'.'.$ext);
+    $filePath = $globalPath->applyPathPrefix($file);
+
+    Storage::makeDirectory('public/'.$destFolder, 0777, true);
+
+    // eseguo il comando FFMPEG
+    $cli = FFMPEG_LIB.' -i '.$filePath.' '.storage_path('app/public/'.$destFolder).$filename.'.mp4';
+    exec($cli);
+
+    // Cancello il file temporaneo
+    Storage::delete($file);
+
+    // salvo la path del file converito per il DB
+    $path = $destFolder.$filename.'.mp4';
+
+
+    // get duration
+    $filePath = $globalPath->applyPathPrefix('public/'.$path);
+    $cli = FFMPEG_LIB.' -i '.$filePath.' 2>&1 | grep \'Duration\' | cut -d \' \' -f 4 | sed s/,//';
+    $duration =  exec($cli);
+
+    // Converto la durata in secondi
+    $duration = explode(":",$duration);
+    $duration = $duration[0]*3600 + $duration[1]*60+ round($duration[2]);
+    $timeToSnap = $duration / 2;
+
+    // prendo il frame e lo salvo
+    $cli = FFMPEG_LIB.' -y -i '.$filePath.' -f mjpeg -vframes 1 -ss '.$timeToSnap.' '.storage_path('app/public/'.$destFolder).$filename.'-thumb.jpg';
+    exec($cli);
+
+    // salvo la path del frame
+    $thumbPath = $destFolder.$filename.'-thumb.jpg';
+
+    $data = [
+      'src' => $path,
+      'img' => $thumbPath
+    ];
+
+    return $data;
   }
 }
