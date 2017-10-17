a2cb8cc5aa82dc6579fcc8e3edd84456095655da
simone pozzobon <info@simonepozzobon.com>
2017-10-18 00:03:13 +0200

    Admin image view

---
+++ resources/views/admin/image/index.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.admin')
+@section('title', 'Video')
+@section('content')
+<div id="app">
+  <div class="row">
+    <div class="col">
+      <div class="box container-fluid mb-4">
+        <div class="row">
+          <div class="col dark-blue py-3 px-5">
+            <h3>Nuovo Video</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col blue p-5">
+            <video-form-upload
+                action=""
+                method="POST"
+                token="{{ csrf_token() }}"
+                options="{{ $categories }}"
+                sections="{{ $sections }}"
+                app_categories="{{ $app_categories }}"
+                apps="{{ $apps }}"
+              >
+            </video-form-upload>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <div class="box container-fluid mb-4">
+        <div class="row">
+          <div class="col dark-orange py-3 px-5">
+            <h3>Video Caricati</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col orange p-5">
+            <video-crud items="{{ $videos }}" token="{{ csrf_token() }}"></video-crud>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
+@section('scripts')
+  <script src="{{ mix('js/admin/video.js') }}"></script>
+@endsection
