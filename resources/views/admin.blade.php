fcaff3456754989438b604e14b86885cbeb2665c
simone pozzobon <info@simonepozzobon.com>
2017-10-17 19:55:27 +0200

    New Admin panel views

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,50 +1,48 @@
 @extends('layouts.admin')
-@section('title', 'Panel')
+@section('title', 'Statistiche')
 @section('content')
-<div id="app">
-  <div class="row">
-    <div class="col">
-      <div class="box container-fluid mb-4">
-        <div class="row">
-          <div class="col dark-blue py-3 px-5">
-            <h3>Nuovo Video</h3>
-          </div>
+<div class="row">
+  <div class="col-md-4">
+    <div class="box container-fluid mb-4">
+      <div class="row">
+        <div class="col dark-blue py-3 px-5">
+          <h3>Utenti Online</h3>
         </div>
-        <div class="row">
-          <div class="col blue p-5">
-            <video-form-upload
-                action=""
-                method="POST"
-                token="{{ csrf_token() }}"
-                options="{{ $categories }}"
-                sections="{{ $sections }}"
-                app_categories="{{ $app_categories }}"
-                apps="{{ $apps }}"
-              >
-            </video-form-upload>
-          </div>
+      </div>
+      <div class="row">
+        <div class="col blue p-5">
+          <h1>{{ $users->count() }}</h1>
         </div>
       </div>
     </div>
   </div>
-  <div class="row">
-    <div class="col">
-      <div class="box container-fluid mb-4">
-        <div class="row">
-          <div class="col dark-orange py-3 px-5">
-            <h3>Video Caricati</h3>
-          </div>
+  <div class="col-md-4">
+    <div class="box container-fluid mb-4">
+      <div class="row">
+        <div class="col dark-blue py-3 px-5">
+          <h3>Sessioni</h3>
         </div>
-        <div class="row">
-          <div class="col orange p-5">
-            <video-crud items="{{ $videos }}" token="{{ csrf_token() }}"></video-crud>
-          </div>
+      </div>
+      <div class="row">
+        <div class="col blue p-5">
+          <h1>{{ $sessions->count() }}</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="box container-fluid mb-4">
+      <div class="row">
+        <div class="col dark-blue py-3 px-5">
+          <h3>Visualizzazioni</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col blue p-5">
+          <h1>{{ $page_views_tot }}</h1>
         </div>
       </div>
     </div>
   </div>
 </div>
-@endsection
-@section('scripts')
-  <script src="{{ mix('js/admin/test.js') }}"></script>
 @endsection
