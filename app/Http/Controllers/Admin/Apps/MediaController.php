8bc2fc3f437c88402f8437f0ca9e41b3b56106cc
simone pozzobon <info@simonepozzobon.com>
2017-10-17 23:49:07 +0200

    Controller and api routes

---
+++ app/Http/Controllers/Admin/Apps/MediaController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Apps;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MediaController extends Controller
+{
+    public function uploadMedia()
+    {
+
+    }
+
+    public function deleteVideo()
+    {
+      
+    }
+}
--- app/Http/Controllers/Admin/ImageController.php
+++ app/Http/Controllers/Admin/ImageController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\App;
+use App\Media;
+use App\AppSection;
+use App\AppCategory;
+use App\MediaCategory;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -9,6 +14,21 @@ class ImageController extends Controller
 {
     public function adminImage()
     {
+      $categories = MediaCategory::all();
+      $sections = AppSection::all();
+      $app_categories = AppCategory::all();
+      $apps = App::all();
+      $medias = Media::orderBy('id', 'desc')->with('apps')->get();
+
+      foreach ($medias as $key => $media) {
+        $media->img = Storage::disk('local')->url($media->img);
+        $app = $media->apps()->first();
+        $category = $app->category()->first();
+        $pavilion = $category->section()->first();
+        $media->path = $pavilion->name.' > '.$category->name.' > '.$app->title;
+      }
+
       dd('immagini');
+      return view('admin.media.index', compact('categories', 'sections', 'app_categories', 'apps', 'medias'));
     }
 }
