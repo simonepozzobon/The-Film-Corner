55e2c6e496f6b3185a1ef37a81643003165d5373
simone pozzobon <info@simonepozzobon.com>
2017-10-24 11:18:17 +0200

    change socket ip

--- resources/assets/js/socket-test.js
+++ resources/assets/js/socket-test.js
@@ -3,7 +3,7 @@ import VueSocketio from 'vue-socket.io'
 import axios from 'axios'
 import $ from 'jquery';
 
-Vue.use(VueSocketio, 'http://localhost:6001')
+Vue.use(VueSocketio, 'http://127.0.0.1:6001')
 import tfcChat from './components/tfcChat.vue'
 
 
