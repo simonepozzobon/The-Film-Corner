3d80716e8eb5ebace28dfaa79cc62eed2b07ea3c
simone pozzobon <info@simonepozzobon.com>
2017-11-29 20:25:43 +0100

    Fixed studio > app_category heading component

--- resources/views/components/apps/heading.blade.php
+++ resources/views/components/apps/heading.blade.php
@@ -1,16 +1,56 @@
   <div class="row mt">
     <div class="col">
       <div class="title-wrapper bg-faded">
+        <div class="icons-left"></div>
         <div class="title">
           {{ $app_category->name }}
         </div>
-        <div class="icon-right">
-            <i class="fa fa-plus" data-toggle="collapse" href="#app-info" aria-expanded="false" aria-controls="app-info"></i>
+        <div class="icons-right">
+          <div class="icon">
+            <a id="read-more-icon"><i class="fa fa-plus"></i></a>
+          </div>
         </div>
       </div>
-      <div id="app-info" class="title-description collapse">
-        {!! $app_category->description !!}
+      <div id="app-info" class="title-description">
+        <div id="short-description" class="short-description">
+          {{ substr(strip_tags($app_category->description), 0, 200) }}{{ strlen(strip_tags($app_category->description)) > 200 ? '...' : "" }}
+        </div>
+        <div id="long-description" class="long-description d-none">
+          {!! $app_category->description !!}
+        </div>
+        <div class="btns">
+          <a href="" id="read-more-btn" class="read-more" data-id="{{ $app_category->slug }}">Read More</a>
+        </div>
       </div>
+      <script type="text/javascript">
+        var button = document.getElementById('read-more-btn'),
+            icon = document.getElementById('read-more-icon'),
+            shortDesc = document.getElementById('short-description'),
+            longDesc = document.getElementById('long-description'),
+            opened = false;
+
+        button.addEventListener('click', toggleDescription, false);
+        icon.addEventListener('click', toggleDescription, false);
+
+        function toggleDescription()
+        {
+            if (opened)
+            {
+                longDesc.classList.remove('d-none');
+                shortDesc.classList.add('d-none');
+                button.innerHTML = 'Read Less'
+            }
+
+            else
+            {
+                shortDesc.classList.remove('d-none');
+                longDesc.classList.add('d-none');
+                button.innerHTML = 'Read More';
+            }
+
+            opened = !opened;
+        }
+      </script>
     </div>
   </div>
   <div class="row mt hidden-md-down">
