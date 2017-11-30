f212edd90755f3e8ffcbf26ed043577d6c985ec8
simone pozzobon <info@simonepozzobon.com>
2017-11-30 12:32:12 +0100

    better styling

--- resources/assets/js/socket-test.js
+++ resources/assets/js/socket-test.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import VueSocketio from 'vue-socket.io'
-import axios from 'axios'
-import $ from 'jquery';
 
 Vue.use(VueSocketio, 'http://'+ window.location.hostname +':6001')
 import tfcChat from './components/tfcChat.vue'
 
 
-const app = new Vue({
+new Vue({
   el: '#app',
   components: {
     tfcChat
   }
-});
+})
