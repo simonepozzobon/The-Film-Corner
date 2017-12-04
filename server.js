1dabe354156b3042dbe2394c33edb7073557f8ce
simone pozzobon <info@simonepozzobon.com>
2017-12-04 03:21:04 +0100

    updated server.js

--- server.js
+++ server.js
@@ -1,37 +1,37 @@
 #!/usr/bin/env nodejs
-var _ = require('lodash');
+// var _ = require('lodash')
 
-var server = require ('http').Server();
-var io = require ('socket.io')(server);
+var server = require ('http').Server()
+var io = require ('socket.io')(server)
 
-io.set('origins', '*:*');
+io.set('origins', '*:*')
 
-var Redis = require ('ioredis');
-var redis = new Redis();
+var Redis = require ('ioredis')
+var redis = new Redis()
 
 
 // canale a cui iscriversi
-redis.subscribe('chat');
-redis.subscribe('notification');
+redis.subscribe('chat')
+redis.subscribe('notification')
 
 // evento quando arriva un messaggio su quel canale
 redis.on('message', (channel, message) => {
-  message = JSON.parse(message);
+  message = JSON.parse(message)
 
   if (channel == 'chat') {
-    // io.emit(channel + ':' + message.event, message.data); //es. chat:UserSignin, data
+    // io.emit(channel + ':' + message.event, message.data) //es. chat:UserSignin, data
     // channel:event:to_id:to_type - message.data
-    io.emit(channel + ':' + message.event + ':' + message.to_id + ':' + message.to_type, message.data);
-    console.log('Messaggio ricevuto per ' + message.to_id + ' ' + message.to_type);
-    // console.log(channel + ':' + message.event + ':' + message.to_id + ':' + message.to_type);
+    io.emit(channel + ':' + message.event + ':' + message.to_id + ':' + message.to_type, message.data)
+    console.log('Messaggio ricevuto per ' + message.to_id + ' ' + message.to_type)
+    // console.log(channel + ':' + message.event + ':' + message.to_id + ':' + message.to_type)
   }
   else if (channel == 'notification') {
     // channel:event:to_id:to_type - message (complete message)
-    io.emit(channel + ':' + message.event + ':' + message.to_id + ':' + message.to_type, message);
-    console.log('Notifica ricevuta per ' + message.to_id + ' ' + message.to_type);
+    io.emit(channel + ':' + message.event + ':' + message.to_id + ':' + message.to_type, message)
+    console.log('Notifica ricevuta per ' + message.to_id + ' ' + message.to_type)
   }
-});
+})
 
-server.listen('6001');
+server.listen('6001')
 
-console.log('Server is running at port 6001');
+console.log('Server is running at port 6001')
