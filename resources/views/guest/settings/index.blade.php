a1c5b896cea2abbd5bca3afb70d32052440dae0a
simone pozzobon <info@simonepozzobon.com>
2017-12-22 17:29:16 +0100

    guest settings page

--- resources/views/guest/settings/index.blade.php
+++ resources/views/guest/settings/index.blade.php
@@ -3,13 +3,11 @@
 @section('content')
   <div id="main" class="container">
     @include('components.apps.heading_simple', ['title' => 'Profile'])
-    <teacher-profile
-      students="{{ $students }}"
-      notifications="{{ $sessions }}"
-      user="{{ Auth::guard('guest')->user() }}"
-      user_type="{{ get_class(Auth::guard('guest')->user()) }}"
-      shared_sessions="{{ $shared_sessions }}"
-    />
+    <div class="box green">
+      <div class="box-body">
+        Not available for guest user!
+      </div>
+    </div>
   </div>
 @endsection
 @section('scripts')
