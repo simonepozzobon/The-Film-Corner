9122155fc6813bac37574e5fb85178e2fa46314b
simone pozzobon <info@simonepozzobon.com>
2018-01-10 19:33:16 +0100

    Added project translation

--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -154,7 +154,7 @@
         </div>
         <div id="project" class="box blue mt">
           <div class="box-header">
-            The Project
+            {{ GeneralText::field('the_project') }}
           </div>
           <div class="box-body">
             <div class="row">
@@ -162,14 +162,7 @@
                 <img src="{{ asset('img/icons/camera.svg') }}" class="img-fluid w-100" >
               </div>
               <div class="col-md-8">
-                “THE FILM CORNER. Online and offline activities for Film Literacy” project is aimed to the design, release and testing of an online digital virtual user-centered platform for Film Literacy, taking advantage of the opportunities offered by web 2.0 and crossmedia innovative approach in the digital era in order to raise the average film literacy level of EU young audiences. The general aim of the project is to contribute to draw an easy-going model for Film Literacy that could improve Film Literacy skills among the audience in order to foster Audience Development and Engagement towards film as an art form, with a particular focus on young and non-core audience.<br>
-                The platform consists of an interactive narrative-based environmental layout with game-based didactical resources integrated as apps the user can interact with. Didactical resources will be both based on generic Film Literacy skills and on a set of EU national and non-national films. The platform is to be developed in at least 4 EU languages including english, french, italian and serbian.<br>
-                In november 2017, an international conference will take place in the frame of the 10th edition of “Piccolo Grande Cinema” Festival, a film festival dedicated to young audience promoted by Fondazione Cineteca Italiana. During the international conference the platform blueprint will be officially presented and some panels discussing several issues concerning Film Literacy, cross-media and Audience Development and engagement will be discussed. A call for papers will be issued and keynote speakers, including representants from other film literacy project financed in the frame of the Creative Europe Programme will be invited.<br>
-                <ul class="pt">
-                  <li>PROJECT TITLE: The Film Corner. New online and offline activities for Film Literacy</li>
-                  <li>CONTRACT REFERENCE: 2016-2127/001-001-577573-CREA-1-2016-1-IT-MED-AUDEV</li>
-                  <li>LEADING INSTITUTION: Fondazione Cineteca Italiana, Milan, Italy</li>
-                </ul>
+                {!! GeneralText::field('the_project_description') !!}
               </div>
             </div>
           </div>
