f37734e249dc26e3fe4b136f89d8e465b0135170
Angus Simons <info@simonepozzobon.com>
2019-06-12 00:51:37 +0200

    SoundStudio e CharacterBuilder loader e session save

--- resources/assets/js/views/apps/CharacterBuilder.vue
+++ resources/assets/js/views/apps/CharacterBuilder.vue
@@ -72,7 +72,8 @@
 <script>
 import {
     fabric
-} from 'fabric'
+}
+from 'fabric'
 import AppTemplate from './AppTemplate.vue'
 import SizeUtility from '../../Sizes'
 import {
@@ -80,17 +81,20 @@ import {
     UiAppLibrary,
     UiAppNote,
     UiAppPreview,
-} from '../../uiapp'
+}
+from '../../uiapp'
 import {
     SharedData,
     SharedMethods,
     SharedWatch
-} from './Shared'
+}
+from './Shared'
 import {
     UiBlock,
     UiImage,
     UiRow,
-} from '../../ui'
+}
+from '../../ui'
 require('gsap/ScrollToPlugin')
 export default {
     name: 'CharacterBuilder',
@@ -113,6 +117,7 @@ export default {
                 clothes: null,
                 people: null,
             },
+            isLoading: false,
         }
     },
     watch: {
@@ -124,15 +129,39 @@ export default {
         ...SharedWatch,
     },
     methods: {
-        init: function () {},
+        init: function () {
+            let session = this.$root.session
+            if (session && session.app_id === 13) {
+                let content = session.content
+                this.notes = content.notes
+                let srcs = content.canvas ? JSON.parse(content.canvas) : null
+                let count = 0
+                if (srcs) {
+                    Object.keys(srcs).forEach(key => {
+                        if (srcs[key]) {
+                            count++
+                        }
+                    })
+                }
+                if (count > 0) {
+                    this.session = session
+                    this.isLoading = true
+                    this.$root.isOpen = true
+                    this.$root.objectsToLoad = count
+                    Object.keys(srcs).forEach(key => {
+                        if (srcs[key]) {
+                            this.srcs[key] = srcs[key]
+                        }
+                    })
+                }
+            }
+        },
         selected: function (index, libraryID) {
             // this.addToCanvas(index, libraryID)
             let library, asset, url, idx
-            library = this.assets.library.find(library => library.id ===
-                libraryID)
+            library = this.assets.library.find(library => library.id === libraryID)
             asset = library.medias.find(asset => asset.id === index)
-            idx = this.assets.library.findIndex(library => library.id ===
-                libraryID)
+            idx = this.assets.library.findIndex(library => library.id === libraryID)
             url = '/storage/' + asset.src
             switch (library.name) {
             case 'Landscapes':
@@ -159,6 +188,10 @@ export default {
             let rowBottom = SizeUtility.get(this.$refs.rowBottom.$el)
             let height = rowTop.hClean + rowBottom.hClean + 48
             this.$refs.library.setLibraryHeight(height)
+
+            if (this.isLoading) {
+                this.$root.objectsLoaded++
+            }
         },
         saveContent: _.debounce(function () {
             let content = this.$root.session.content
@@ -167,8 +200,7 @@ export default {
                 notes: this.notes
             }
             for (let key in content) {
-                if (content.hasOwnProperty(key) && newContent.hasOwnProperty(
-                        key)) {
+                if (content.hasOwnProperty(key) && newContent.hasOwnProperty(key)) {
                     content[key] = newContent[key]
                 }
             }
@@ -176,7 +208,7 @@ export default {
                 ...this.$root.session,
                 content: content
             }
-            console.log(this.$root.session);
+            // console.log(this.$root.session);
         }, 500)
     },
     created: function () {
