23748d0fdd62c43fcd25445610d7fc8781e8e9ad
Angus Simons <info@simonepozzobon.com>
2019-07-25 01:05:22 +0200

    Customizzazione card studios

--- resources/assets/js/views/apps/AppsHome.vue
+++ resources/assets/js/views/apps/AppsHome.vue
@@ -47,12 +47,18 @@
                     />
 
                     <div v-if="studio.slug == 'cultural-approach'">
-                        <ul class="block-menu">
+                        <ul class="block-menu block-menu--var">
                             <li
                                 class="block-menu__menu-head"
                                 @click="goToPropaganda(studio.slug)"
                             >
-                                Propagandapp
+                                Propagand<b>app</b>
+                            </li>
+                            <li
+                                class="block-menu__menu-item"
+                                @click="goToPropaganda(studio.slug)"
+                            >
+                                Go to the challenges
                             </li>
                         </ul>
                     </div>
@@ -193,6 +199,12 @@ export default {
     &__menu-item {
         &:hover {
             text-decoration: underline;
+        }
+    }
+
+    &--var & {
+        &__menu-head {
+            font-weight: 600;
         }
     }
 }
