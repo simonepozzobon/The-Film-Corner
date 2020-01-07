abbda6759b87b5078aef79767b273bd9d7e11633
Angus Simons <info@simonepozzobon.com>
2020-01-07 12:09:11 +0100

    Route per le utilities commentate

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,9 @@ Route::get('/test', 'Api\Admin\ClipsController@test');
 // Route::get('/test', 'Api\SectionController@test');
 // Route::get('/test', 'Api\SectionController@get_network');
 Route::get('/fill-trans', 'Api\TestController@fill_empty_translations');
-Route::get('/convert-session-to-network', 'Api\TestController@convert_shared_sessions_to_networs');
 
-Route::get('/convert-teachers', 'Api\TestController@convert_teacher_to_user');
-Route::get('/convert-students', 'Api\TestController@convert_student_to_user');
+// Route::get('/convert-session-to-network', 'Api\TestController@convert_shared_sessions_to_networs');
+// Route::get('/convert-teachers', 'Api\TestController@convert_teacher_to_user');
+// Route::get('/convert-students', 'Api\TestController@convert_student_to_user');
 Route::get('/admin/{any?}', 'BackendController@home_page')->where('any', '.*')->name('admin');
 Route::get('{any}', 'FrontendController@home_page')->where('any', '.*')->name('home.page');
