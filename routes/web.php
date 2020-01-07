7ca531235a76d3b606801af622bd542a967279ad
Angus Simons <info@simonepozzobon.com>
2020-01-07 11:49:16 +0100

    Aggiunge route per conversione sessioni network

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/test', 'Api\Admin\ClipsController@test');
 // Route::get('/test', 'Api\SectionController@test');
 // Route::get('/test', 'Api\SectionController@get_network');
 Route::get('/fill-trans', 'Api\TestController@fill_empty_translations');
+Route::get('/convert-session-to-network', 'Api\TestController@convert_shared_sessions_to_networs');
 
 Route::get('/convert-teachers', 'Api\TestController@convert_teacher_to_user');
 Route::get('/convert-students', 'Api\TestController@convert_student_to_user');
