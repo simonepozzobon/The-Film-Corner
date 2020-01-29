6c357ae6e9027509ae33ee02f3f94397bcc5760c
Esse <info@simonepozzobon.com>
2020-01-29 09:17:36 +0100

    Aggiunta del valore iniziale per gli esercizi

--- resources/assets/js/admin/components/clips/Esercizi.vue
+++ resources/assets/js/admin/components/clips/Esercizi.vue
@@ -1,155 +1,160 @@
 <template>
 <div class="row no-gutters">
-    <div
-        class="col"
+    <switch-container
+        ref="switch"
         v-for="option in options"
         :key="option.id"
-    >
-        <switch-input
-            ref="switch"
-            :label="option.title"
-            label-size="col-md-6"
-            input-size="col-md-6"
-            :has-row="true"
-            @update="updateSelection($event, option)"
-        />
-    </div>
+        :option="option"
+        @ready="switchReady"
+        @update="updateSelection($event, option)"
+    />
 </div>
 </template>
 
 <script>
-import {
-    SwitchInput,
-}
-from '../../adminui'
+import SwitchContainer from './SwitchContainer.vue'
 
 export default {
-    name: 'Esercizi',
+    name: "Esercizi",
     components: {
-        SwitchInput,
+        SwitchContainer,
     },
     props: {
         options: {
             type: Array,
             default: function () {
-                return []
-            },
+                return [];
+            }
         },
         exercises: {
             type: Array,
             default: function () {
-                return []
+                return [];
             }
         },
         initials: {
             type: Object,
             default: function () {
-                return {}
-            },
+                return {};
+            }
         },
         clip: {
             type: Object,
             default: function () {
-                return {}
-            },
-        },
+                return {};
+            }
+        }
     },
     data: function () {
         return {
             clipId: null,
-            keys: [
-                'exercise_1',
-                'exercise_2',
-                'exercise_3'
-            ]
-        }
+            counter: 0,
+            keys: ["exercise_1", "exercise_2", "exercise_3"]
+        };
     },
     watch: {
         initials: function (initials) {
             // console.log('wqtch', initials);
-            this.setInitials()
+            this.setInitials();
         },
         clip: function () {
-            this.checkClipId()
-        },
+            this.checkClipId();
+        }
     },
     methods: {
         checkClipId: function () {
             // set clip id
-            if (this.clip && this.clip.hasOwnProperty('id') && this.clip.id > 0) {
-                this.clipId = this.clip.id
+            if (
+                this.clip &&
+                this.clip.hasOwnProperty("id") &&
+                this.clip.id > 0
+            ) {
+                this.clipId = this.clip.id;
             }
         },
         updateSelection: function (value, option) {
             if (value) {
-                this.addExercise(option)
+                this.addExercise(option);
             }
             else {
-                this.removeExercise(option)
+                this.removeExercise(option);
             }
         },
         addExercise: function (option) {
-            const newExercise = Object.assign({}, option)
-            newExercise['uuid'] = this.$util.uuid()
-            newExercise['isNew'] = true
+            const newExercise = Object.assign({}, option);
+            newExercise["uuid"] = this.$util.uuid();
+            newExercise["isNew"] = true;
 
-            if (newExercise.hasOwnProperty('has_library') && newExercise.has_library == 1) {
-                newExercise['libraries'] = []
+            if (
+                newExercise.hasOwnProperty("has_library") &&
+                newExercise.has_library == 1
+            ) {
+                newExercise["libraries"] = [];
             }
 
-            let cached = this.exercises
-            cached.push(newExercise)
-
-
+            let cached = this.exercises;
+            cached.push(newExercise);
 
             if (this.clipId) {
-                let data = new FormData()
-                data.append('clip_id', this.clipId)
-                data.append('exercise_id', option.id)
+                let data = new FormData();
+                data.append("clip_id", this.clipId);
+                data.append("exercise_id", option.id);
 
-                this.$http.post('/api/v2/admin/clips/exercises/add', data).then(response => {
-                    // console.log('response add', response);
-                    this.$emit('update:exercises', cached)
-                })
+                this.$http
+                    .post("/api/v2/admin/clips/exercises/add", data)
+                    .then(response => {
+                        // console.log('response add', response);
+                        this.$emit("update:exercises", cached);
+                    });
             }
             else {
-                this.$emit('update:exercises', cached)
+                this.$emit("update:exercises", cached);
             }
         },
         removeExercise: function (option) {
-            let idx = this.exercises.findIndex(exercise => exercise.id == option.id)
+            let idx = this.exercises.findIndex(
+                exercise => exercise.id == option.id
+            );
             if (idx > -1) {
-                let cached = this.exercises
-                cached.splice(idx, 1)
+                let cached = this.exercises;
+                cached.splice(idx, 1);
 
                 if (this.clipId) {
-                    let data = new FormData()
-                    data.append('clip_id', this.clipId)
-                    data.append('exercise_id', option.id)
+                    let data = new FormData();
+                    data.append("clip_id", this.clipId);
+                    data.append("exercise_id", option.id);
 
-                    this.$http.post('/api/v2/admin/clips/exercises/remove', data).then(response => {
-                        // console.log('response remove', response);
-                        this.$emit('update:exercises', cached)
-                    })
+                    this.$http
+                        .post("/api/v2/admin/clips/exercises/remove", data)
+                        .then(response => {
+                            // console.log('response remove', response);
+                            this.$emit("update:exercises", cached);
+                        });
                 }
                 else {
-                    this.$emit('update:exercises', cached)
+                    this.$emit("update:exercises", cached);
                 }
             }
             else {
                 // console.log('non trovato');
             }
         },
+        switchReady: function () {
+            this.counter = this.counter + 1
+            if (this.counter >= 3) {
+                this.setInitials()
+            }
+        },
         setInitials: function () {
             if (this.initials) {
                 for (let i = 0; i < this.keys.length; i++) {
-                    let key = this.keys[i]
+                    let key = this.keys[i];
                     if (this.initials.hasOwnProperty(key)) {
-                        let idx = parseInt(key.replace('exercise_', '')) - 1
-
+                        let idx = parseInt(key.replace("exercise_", "")) - 1;
                         if (this.$refs.switch) {
-                            let component = this.$refs.switch[idx]
-                            component.value = this.initials[key] == 1 ? true : false
+                            let component = this.$refs.switch[idx];
+                            console.log(component);
+                            component.value = this.initials[key] == 1 ? true : false;
                         }
                     }
                 }
@@ -157,12 +162,11 @@ export default {
         }
     },
     mounted: function () {
-        this.checkClipId()
-        this.setInitials()
-    },
-}
+        this.checkClipId();
+    }
+};
 </script>
 
 <style lang="scss" scoped>
-@import '~styles/shared';
+@import "~styles/shared";
 </style>
---
+++ resources/assets/js/admin/components/clips/SwitchContainer.vue
@@ -0,0 +1,57 @@
+<template>
+<div class="col">
+    <switch-input
+        ref="switch"
+        :label="option.title"
+        label-size="col-md-6"
+        input-size="col-md-6"
+        :has-row="true"
+        @update="updateSelection($event, option)"
+    />
+</div>
+</template>
+
+<script>
+import {
+    SwitchInput
+}
+from '../../adminui'
+export default {
+    name: 'SwitchContainer',
+    components: {
+        SwitchInput
+    },
+    props: {
+        option: {
+            type: Object,
+            default: function () {
+                return {}
+            },
+        },
+    },
+    data: function () {
+        return {
+            value: null
+        }
+    },
+    watch: {
+        value: function (value) {
+            this.$refs.switch.value = value
+        }
+    },
+    methods: {
+        updateSelection: function ($event, option) {
+            this.$emit('updateSelection', $event, option)
+        }
+    },
+    mounted: function () {
+        this.$nextTick(() => {
+            this.$emit('ready')
+        })
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~styles/shared';
+</style>
