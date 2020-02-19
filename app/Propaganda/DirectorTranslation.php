004ebe9d3e611a57a60db23340bc8e620ed3c291
Esse <info@simonepozzobon.com>
2020-02-19 11:21:38 +0100

    Model Translations

--- app/Propaganda/DirectorTranslation.php
+++ app/Propaganda/DirectorTranslation.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class DirectorTranslation extends Model
 {
-    //
+    public $timestamps = false;
+    protected $connection = 'tfc_propaganda';
+    protected $fillable = ['name'];
+
+    public static function get_db_table()
+    {
+        return with(new static)->getTable();
+    }
 }
--- app/Propaganda/PeriodTranslation.php
+++ app/Propaganda/PeriodTranslation.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class PeriodTranslation extends Model
 {
-    //
+    public $timestamps = false;
+    protected $connection = 'tfc_propaganda';
+    protected $fillable = ['title'];
+
+    public static function get_db_table()
+    {
+        return with(new static)->getTable();
+    }
 }
