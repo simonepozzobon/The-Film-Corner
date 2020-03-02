b0a119cb2118ea06080ca8bd8c53cc1894031ac7
Angus Simons <info@simonepozzobon.com>
2020-03-02 02:06:21 +0100

    Traduzioni tags, genere e formato

--- resources/assets/js/uiapp/sub/search-results/SingleResult.vue
+++ resources/assets/js/uiapp/sub/search-results/SingleResult.vue
@@ -1,138 +1,138 @@
 <template>
-<div class="ua-single-res">
-    <div class="ua-single-res__container">
-        <div
-            class="ua-single-res__thumb"
-            @click="openResult"
-        >
-            <div class="ua-single-res__thumb-container">
-                <ui-image
-                    v-if="content.thumb"
-                    :src="content.thumb"
-                    :alt="content | translate('title', $root.locale)"
-                    :has-margin="false"
-                />
-                <div class="overlay">
-                    <single-hover />
-                </div>
-                <!-- <video
+    <div class="ua-single-res">
+        <div class="ua-single-res__container">
+            <div class="ua-single-res__thumb" @click="openResult">
+                <div class="ua-single-res__thumb-container">
+                    <ui-image
+                        v-if="content.thumb"
+                        :src="content.thumb"
+                        :alt="content | translate('title', $root.locale)"
+                        :has-margin="false"
+                    />
+                    <div class="overlay">
+                        <single-hover />
+                    </div>
+                    <!-- <video
                     ref="player"
                     class="video-js d-none"
                 ></video> -->
-            </div>
-        </div>
-        <div class="ua-single-res__details">
-            <div class="ua-single-res__title">
-                <ui-title
-                    :title="content | translate('title', $root.locale)"
-                    :has-container="false"
-                    :has-padding="false"
-                    :has-margin="false"
-                    tag="h3"
-                    font-size="h6"
-                />
-            </div>
-            <div class="ua-single-res__year">
-                {{ content.year }}
-            </div>
-            <div class="ua-single-res__director">
-                <span
-                    v-for="(director, i) in content.directors"
-                    :key="director.id"
-                >
-                    <span v-if="i < content.directors.length - 1">{{ director.name }}, </span>
-                    <span v-else>{{ director.name }}</span>
-                </span>
-            </div>
-            <div class="ua-single-res__country">
-                <span v-if="content.duration">
-                    {{ content.nationality }}, {{ content.duration }}'
-                </span>
-                <span v-else>
-                    {{ content.nationality }}
-                </span>
-            </div>
-            <div class="ua-single-res__attributes">
-                <div class="ua-single-res__attribute">
-                    <span>{{ content.format.title }}</span>
-
                 </div>
             </div>
-            <div class="ua-single-res__tags">
-                <div
-                    class="ua-single-res__tag"
-                    v-for="(topic, i) in content.topics"
-                    :key="topic.id"
-                >
-                    <span v-if="i != content.topics.length - 1 ">
-                        {{ topic.title }},&nbsp;
-                    </span>
-                    <span v-else>
-                        {{ topic.title }}
+            <div class="ua-single-res__details">
+                <div class="ua-single-res__title">
+                    <ui-title
+                        :title="content | translate('title', $root.locale)"
+                        :has-container="false"
+                        :has-padding="false"
+                        :has-margin="false"
+                        tag="h3"
+                        font-size="h6"
+                    />
+                </div>
+                <div class="ua-single-res__year">
+                    {{ content.year }}
+                </div>
+                <div class="ua-single-res__director">
+                    <span
+                        v-for="(director, i) in content.directors"
+                        :key="director.id"
+                    >
+                        <span v-if="i < content.directors.length - 1">
+                            {{ director | translate("name", $root.locale) }},
+                            &nbsp;
+                        </span>
+                        <span v-else>
+                            {{ director | translate("name", $root.locale) }}
+                        </span>
                     </span>
                 </div>
-            </div>
-            <div
-                class="ua-single-res__tags"
-                v-if="content.tags"
-            >
-                <div
-                    class="ua-single-res__tag"
-                    v-for="(tag, i) in content.tags"
-                    :key="tag.id"
-                >
-                    <span v-if="i != content.tags.length - 1 ">
-                        {{ tag.title }},&nbsp;
+                <div class="ua-single-res__country">
+                    <span v-if="content.duration">
+                        {{ content.nationality }}, {{ content.duration }}'
                     </span>
                     <span v-else>
-                        {{ tag.title }}
+                        {{ content.nationality }}
                     </span>
+                </div>
+                <div class="ua-single-res__attributes">
+                    <div class="ua-single-res__attribute">
+                        <span>
+                            {{
+                                content.format
+                                    | translate("title", $root.locale)
+                            }}
+                        </span>
+                    </div>
+                </div>
+                <div class="ua-single-res__tags">
+                    <div
+                        class="ua-single-res__tag"
+                        v-for="(topic, i) in content.topics"
+                        :key="topic.id"
+                    >
+                        <span v-if="i != content.topics.length - 1">
+                            {{ topic | translate("title", $root.locale) }},
+                            &nbsp;
+                        </span>
+                        <span v-else>
+                            {{ topic | translate("title", $root.locale) }}
+                        </span>
+                    </div>
+                </div>
+                <div class="ua-single-res__tags" v-if="content.tags">
+                    <div
+                        class="ua-single-res__tag"
+                        v-for="(tag, i) in content.tags"
+                        :key="tag.id"
+                    >
+                        <span v-if="i != content.tags.length - 1">
+                            {{ tag | translate("title", $root.locale) }},&nbsp;
+                        </span>
+                        <span v-else>
+                            {{ tag | translate("title", $root.locale) }}
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 </template>
 
 <script>
-import {
-    UiImage,
-    UiTitle,
-}
-from '../../../ui'
+import { UiImage, UiTitle } from "../../../ui";
 
-import SingleHover from './SingleHover.vue'
-import videojs from 'video.js'
-import TranslationFilter from '../../../TranslationFilter'
+import SingleHover from "./SingleHover.vue";
+import videojs from "video.js";
+import TranslationFilter from "../../../TranslationFilter";
 
 export default {
-    name: 'SingleResult',
+    name: "SingleResult",
     mixins: [TranslationFilter],
     components: {
         SingleHover,
         UiImage,
-        UiTitle,
+        UiTitle
     },
     props: {
         content: {
             type: Object,
-            default: function () {
-                return {}
-            },
-        },
+            default: function() {
+                return {};
+            }
+        }
     },
-    data: function () {
+    data: function() {
         return {
             posterSrc: null,
             playerReady: false,
             options: {
-                aspectRatio: '16:9',
+                aspectRatio: "16:9",
                 controls: true,
-                preload: 'auto',
+                preload: "auto",
                 width: 480,
-                height: 270,
+                height: 270
             }
-        }
+        };
     },
     // watch: {
     //     'content.src': function (src) {
@@ -142,55 +142,57 @@ export default {
     //     },
     // },
     methods: {
-        initPlayer: function () {
+        initPlayer: function() {
             this.player = videojs(this.$refs.player, this.options, () => {
-                this.playerReady = true
+                this.playerReady = true;
                 if (this.content.video) {
-                    this.setSrc()
+                    this.setSrc();
                 }
-            })
-            this.player.on('loadeddata', () => {
-                this.getThumbnail()
-            })
+            });
+            this.player.on("loadeddata", () => {
+                this.getThumbnail();
+            });
         },
-        getThumbnail: function () {
-            let player = this.player.tech({
-                IWillNotUseThisInPlugins: true
-            }).el()
+        getThumbnail: function() {
+            let player = this.player
+                .tech({
+                    IWillNotUseThisInPlugins: true
+                })
+                .el();
 
-            let canvas = document.createElement('canvas')
-            canvas.height = 270
-            canvas.width = 480
+            let canvas = document.createElement("canvas");
+            canvas.height = 270;
+            canvas.width = 480;
 
-            let ctx = canvas.getContext('2d')
-            ctx.drawImage(player, 0, 0)
+            let ctx = canvas.getContext("2d");
+            ctx.drawImage(player, 0, 0);
 
-            this.posterSrc = canvas.toDataURL()
+            this.posterSrc = canvas.toDataURL();
             console.log(this.posterSrc);
         },
-        openResult: function () {
-            this.$emit('open-result', this.content)
+        openResult: function() {
+            this.$emit("open-result", this.content);
         },
-        setSrc: function () {
+        setSrc: function() {
             this.player.src({
-                type: 'video/mp4',
+                type: "video/mp4",
                 src: this.content.video
-            })
-        },
+            });
+        }
     },
-    mounted: function () {
+    mounted: function() {
         // this.initPlayer()
-    },
+    }
     // beforeDestroy: function () {
     //     if (this.player) {
     //         this.player.dispose()
     //     }
     // }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-@import '~styles/shared';
+@import "~styles/shared";
 
 .ua-single-res {
     margin-bottom: $spacer * 2;
@@ -237,6 +239,8 @@ export default {
     &__tags {
         display: flex;
         justify-content: flex-start;
+        flex-wrap: wrap;
+        line-height: 1rem;
     }
 
     &:hover & {
