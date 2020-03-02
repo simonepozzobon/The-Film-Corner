1abfc446f42b7d1d6ed97b5615b0b14ff31eeb4d
Angus Simons <info@simonepozzobon.com>
2020-03-02 02:51:58 +0100

    Aggiunta del campo note in Compare the clips

--- resources/assets/js/views/apps/propagandapp/exercises/PropagandaCompareClips.vue
+++ resources/assets/js/views/apps/propagandapp/exercises/PropagandaCompareClips.vue
@@ -1,77 +1,77 @@
 <template>
-<propaganda-exercise-template
-    :content="content"
-    :clip="clip"
-    v-if="content && clip"
->
-    <template slot="preview">
-        <div class="prop-compare-clips">
-            <div class="prop-compare-clips__preview">
-                <div class="prop-compare-clips__left">
-                    <ui-app-propaganda-player
-                        ref="player"
-                        class="prop-compare-clips__player"
-                        color="dark-gray"
-                        :has-age="false"
-                        :title="clip | translate('title', $root.locale)"
-                        title-align="center"
-                        :src="clip.video"
-                        :clip="clip"
-                        :captions="clip.captions"
-                        :has-info="true"
-                        @open-info="openInfo"
-                    />
+    <propaganda-exercise-template
+        :content="content"
+        :clip="clip"
+        v-if="content && clip"
+    >
+        <template slot="preview">
+            <div class="prop-compare-clips">
+                <div class="prop-compare-clips__preview">
+                    <div class="prop-compare-clips__left">
+                        <ui-app-propaganda-player
+                            ref="player"
+                            class="prop-compare-clips__player"
+                            color="dark-gray"
+                            :has-age="false"
+                            :title="clip | translate('title', $root.locale)"
+                            title-align="center"
+                            :src="clip.video"
+                            :clip="clip"
+                            :captions="clip.captions"
+                            :has-info="true"
+                            @open-info="openInfo"
+                        />
+                    </div>
+                    <div class="prop-compare-clips__right">
+                        <ui-app-propaganda-player
+                            ref="right"
+                            class="prop-compare-clips__player"
+                            color="dark-gray"
+                            :has-age="false"
+                            :title="compare | translate('title', $root.locale)"
+                            title-align="center"
+                            :src="compare.video ? compare.video : compare.url"
+                            :clip="compare"
+                            :captions="compare.captions"
+                            :has-info="true"
+                            @open-info="openInfo"
+                        />
+                    </div>
                 </div>
-                <div class="prop-compare-clips__right">
-                    <ui-app-propaganda-player
-                        ref="right"
-                        class="prop-compare-clips__player"
-                        color="dark-gray"
-                        :has-age="false"
-                        :title="compare | translate('title', $root.locale)"
-                        title-align="center"
-                        :src="compare.video ? compare.video : compare.url"
-                        :clip="compare"
-                        :captions="compare.captions"
-                        :has-info="true"
-                        @open-info="openInfo"
+                <div class="prop-compare-clips__library" v-if="movies">
+                    <slider-library
+                        :movies="movies"
+                        @change-video="changeVideo"
+                        @open-modal="openModal"
                     />
+                    <slider-modal-panel ref="modal" :modal="modal" />
                 </div>
             </div>
-            <div
-                class="prop-compare-clips__library"
-                v-if="movies"
-            >
-                <slider-library
-                    :movies="movies"
-                    @change-video="changeVideo"
-                    @open-modal="openModal"
-                />
-                <slider-modal-panel
-                    ref="modal"
-                    :modal="modal"
-                />
-            </div>
-        </div>
-    </template>
-</propaganda-exercise-template>
+        </template>
+        <ui-app-note
+            class="mt-4"
+            color="yellow"
+            @changed="updateNote"
+            :initial="notes"
+        />
+    </propaganda-exercise-template>
 </template>
 
 <script>
-import Utility from '../../../../Utilities'
-import TranslationFilter from '../../../../TranslationFilter'
-import PropagandaExerciseTemplate from './PropagandaExerciseTemplate.vue'
-import SliderLibrary from '../../../../uiapp/sub/propaganda/SliderLibrary.vue'
-import SliderModalPanel from '../../../../uiapp/sub/propaganda/SliderModalPanel.vue'
+import Utility from "../../../../Utilities";
+import TranslationFilter from "../../../../TranslationFilter";
+import PropagandaExerciseTemplate from "./PropagandaExerciseTemplate.vue";
+import SliderLibrary from "../../../../uiapp/sub/propaganda/SliderLibrary.vue";
+import SliderModalPanel from "../../../../uiapp/sub/propaganda/SliderModalPanel.vue";
 
 import {
     UiAppPropagandaPlayer,
     UiAppCroppedFrames,
-}
-from '../../../../uiapp'
+    UiAppNote
+} from "../../../../uiapp";
 
 export default {
-    name: 'PropagandaCompareClips',
+    name: "PropagandaCompareClips",
     mixins: [TranslationFilter],
     components: {
         PropagandaExerciseTemplate,
@@ -79,8 +79,9 @@ export default {
         SliderModalPanel,
         UiAppPropagandaPlayer,
         UiAppCroppedFrames,
+        UiAppNote
     },
-    data: function () {
+    data: function() {
         return {
             content: null,
             clip: null,
@@ -89,146 +90,162 @@ export default {
             movies: [],
             modal: null,
             session: null,
-        }
+            notes: null
+        };
     },
     watch: {
-        session: function (session) {
-            this.$root.session = Object.assign({}, session)
+        session: function(session) {
+            this.$root.session = Object.assign({}, session);
         },
-        compare: function (clip) {
-            let session = Object.assign({}, this.session)
-            session.content['compare'] = clip
-            this.session = session
+        compare: function(clip) {
+            let session = Object.assign({}, this.session);
+            session.content["compare"] = clip;
+            this.session = session;
         },
+        notes: function(notes) {
+            let session = Object.assign({}, this.session);
+            session.content["notes"] = notes;
+            this.session = session;
+        }
     },
     computed: {
-        player: function () {
-            return this.$refs.player.player
+        player: function() {
+            return this.$refs.player.player;
         },
-        playerRight: function () {
-            return this.$refs.right.player
-        },
+        playerRight: function() {
+            return this.$refs.right.player;
+        }
     },
     methods: {
-        uniqid: function () {
+        updateNote: function(notes) {
+            this.notes = notes;
+        },
+        uniqid: function() {
             let ts = String(new Date().getTime()),
                 i = 0,
-                out = ''
+                out = "";
             for (i = 0; i < ts.length; i += 2) {
-                out += Number(ts.substr(i, 2)).toString(36)
+                out += Number(ts.substr(i, 2)).toString(36);
             }
-            return ('d' + out)
+            return "d" + out;
         },
-        uniqidSimple: function () {
-            return '_' + Math.random().toString(36).substr(2, 9)
+        uniqidSimple: function() {
+            return (
+                "_" +
+                Math.random()
+                    .toString(36)
+                    .substr(2, 9)
+            );
         },
-        getData: function () {
-            window.addEventListener('beforeunload', () => {
+        getData: function() {
+            window.addEventListener("beforeunload", () => {
                 try {
-                    this.deleteEmptySession()
+                    this.deleteEmptySession();
+                } catch (e) {
+                } finally {
                 }
-                catch (e) {
+            });
 
-                }
-                finally {
-
-                }
-            })
-
-            let id = this.$route.params.id
-            let exerciseId = this.$route.params.exerciseId
+            let id = this.$route.params.id;
+            let exerciseId = this.$route.params.exerciseId;
             // perform api call
-            let url = '/api/v2/propaganda/clip/' + id + '/exercise/' + exerciseId
+            let url = `/api/v2/propaganda/clip/${id}/exercise/${exerciseId}`;
 
             // console.log('session', this.$root.session);
-            if (this.$root.session && this.$root.session.app_id == 20 && this.$root.session.token) {
-                url = '/api/v2/propaganda/clip/' + id + '/exercise/' + exerciseId + '/' + this.$root.session.token
+            if (
+                this.$root.session &&
+                this.$root.session.app_id == 20 &&
+                this.$root.session.token
+            ) {
+                url = `/api/v2/propaganda/clip/${id}/exercise/${exerciseId}/${this.$root.session.token}`;
             }
 
-
-            // url = '/api/v2/propaganda/clip/' + id + '/exercise/' + exerciseId + '/5e56141cc778e'
+            // url = `/api/v2/propaganda/clip/${id}/exercise/${exerciseId}/5e5c64cc4155d`;
             this.$http.get(url).then(response => {
-                // console.log(response.data);
-                const {
-                    clip,
-                    exercise,
-                    session
-                } = response.data
+                // console.log("sessione", response.data);
+                const { clip, exercise, session } = response.data;
 
-                this.clip = clip
+                this.clip = clip;
                 // this.compare = clip
-                this.content = exercise
-                this.movies = exercise.library.medias
+                this.content = exercise;
+                this.movies = exercise.library.medias;
 
-                let formattedSession = session
-                let content = session.content ? JSON.parse(session.content) : {}
+                let formattedSession = session;
+                let content = session.content
+                    ? JSON.parse(session.content)
+                    : {};
 
-                if (!content.hasOwnProperty('compare')) {
-                    this.compare = clip
+                console.log(content);
+
+                if (!content.hasOwnProperty("compare")) {
+                    this.compare = clip;
+                } else {
+                    this.compare = content.compare;
                 }
-                else {
-                    this.compare = content.compare
+
+                if (content.hasOwnProperty("notes")) {
+                    this.notes = content.notes;
                 }
 
                 formattedSession.content = {
                     ...content
-                }
+                };
 
-                this.session = Object.assign({}, formattedSession)
-                this.$nextTick(this.init)
-            })
+                this.session = Object.assign({}, formattedSession);
+                this.$nextTick(this.init);
+            });
 
             // this.clip = movies.find(movie => movie.id == id)
             // this.compare = this.clip
             // this.content = this.clip.exercises.find(exercise => exercise.id == exerciseId)
         },
-        init: function () {
+        init: function() {
             // if (this.session.content.compare) {
             //     // this.compare = this.session.content.compare
             //     console.log(this.session.content.compare);
             // }
         },
-        deleteEmptySession: function () {
+        deleteEmptySession: function() {
             // verificare se è vuota
             if (Boolean(this.session.is_empty)) {
-                this.$http.delete('/api/v2/session/' + this.session.token + '/true')
+                this.$http.delete(`/api/v2/session/${this.session.token}/true`);
             }
             this.$nextTick(() => {
-                this.$root.session = null
-            })
+                this.$root.session = null;
+            });
         },
-        changeVideo: function (movie) {
+        changeVideo: function(movie) {
             // console.log('compare changed');
-            this.compare = Object.assign({}, movie)
+            this.compare = Object.assign({}, movie);
             // console.log(this.compare);
         },
-        openModal: function (modal) {
-            this.modal = Object.assign({}, modal)
+        openModal: function(modal) {
+            this.modal = Object.assign({}, modal);
             this.$nextTick(() => {
-                this.$refs.modal.show()
-            })
+                this.$refs.modal.show();
+            });
         },
-        openInfo: function (modal) {
-            this.modal = Object.assign({}, modal)
+        openInfo: function(modal) {
+            this.modal = Object.assign({}, modal);
             this.$nextTick(() => {
-                this.$refs.modal.show()
-            })
+                this.$refs.modal.show();
+            });
         }
     },
-    created: function () {
-        this.$root.isApp = true
+    created: function() {
+        this.$root.isApp = true;
     },
-    mounted: function () {
-        this.getData()
+    mounted: function() {
+        this.getData();
     },
-    beforeDestroy: function () {
-        this.$root.isApp = false
-    },
-}
+    beforeDestroy: function() {
+        this.$root.isApp = false;
+    }
+};
 </script>
 
 <style lang="scss" scoped>
-@import '~styles/shared';
+@import "~styles/shared";
 
 .prop-compare-clips {
     margin-top: $spacer * 2;
